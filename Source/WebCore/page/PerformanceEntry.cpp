fberlakovich/r2c-webkit

Implement PerformanceObserver https://bugs.webkit.org/show_bug.cgi?id=167546 <rdar://problem/30247959>
Reviewed by Ryosuke Niwa.

Source/JavaScriptCore:

* runtime/CommonIdentifiers.h:

Source/WebCore:

This implements PerformanceObserver from Performance Timeline Level 2:
https://w3c.github.io/performance-timeline/

Tests: performance-api/performance-observer-api.html
       performance-api/performance-observer-basic.html
       performance-api/performance-observer-callback-mutate.html
       performance-api/performance-observer-callback-task.html
       performance-api/performance-observer-entry-sort.html
       performance-api/performance-observer-exception.html
       performance-api/performance-observer-nested.html
       performance-api/performance-observer-order.html
       performance-api/performance-observer-periodic.html
       performance-api/performance-timeline-api.html

* CMakeLists.txt:
* DerivedSources.make:
* WebCore.xcodeproj/project.pbxproj:
New files.

* page/Performance.h:
* page/Performance.cpp:
(WebCore::Performance::mark):
(WebCore::Performance::measure):
(WebCore::Performance::registerPerformanceObserver):
(WebCore::Performance::unregisterPerformanceObserver):
(WebCore::Performance::queueEntry):
Register PerformanceObservers with the Performance object.
When new PerformanceEntries are created (Mark and Measure
right now) check them against observers.

* page/PerformanceEntry.cpp:
(WebCore::PerformanceEntry::PerformanceEntry):
(WebCore::PerformanceEntry::typeForEntryTypeString):
* page/PerformanceEntry.h:
(WebCore::PerformanceEntry::type):
Give PerformanceEntry a convenience enum for easy comparison
and to know if it is one of the built-in known types (which the
PerformanceObserver API takes into account).

* page/PerformanceObserver.cpp: Added.
(WebCore::PerformanceObserver::PerformanceObserver):
(WebCore::PerformanceObserver::observe):
(WebCore::PerformanceObserver::disconnect):
(WebCore::PerformanceObserver::queueEntry):
(WebCore::Performa

Source/WebCore/page/PerformanceEntry.cpp
@@ -33,18 +33,41 @@
 
 #if ENABLE(WEB_TIMING)
 
+#include "RuntimeEnabledFeatures.h"
+
 namespace WebCore {
 
-PerformanceEntry::PerformanceEntry(const String& name, const String& entryType, double startTime, double finishTime)
+PerformanceEntry::PerformanceEntry(Type type, const String& name, const String& entryType, double startTime, double finishTime)
     : m_name(name)
     , m_entryType(entryType)
     , m_startTime(startTime)
     , m_duration(finishTime - startTime)
+    , m_type(type)
 {
 }
 
 PerformanceEntry::~PerformanceEntry()
 {
+}
+
+std::optional<PerformanceEntry::Type> PerformanceEntry::parseEntryTypeString(const String& entryType)
+{
+    if (entryType == "navigation")
+        return std::optional<Type>(Type::Navigation);
+
+    if (RuntimeEnabledFeatures::sharedFeatures().userTimingEnabled()) {
+        if (entryType == "mark")
+            return std::optional<Type>(Type::Mark);
+        if (entryType == "measure")
+            return std::optional<Type>(Type::Measure);
+    }
+
+    if (RuntimeEnabledFeatures::sharedFeatures().resourceTimingEnabled()) {
+        if (entryType == "resource")
+            return std::optional<Type>(Type::Resource);
+    }
+
+    return std::nullopt;
 }
 
 } // namespace WebCore

Source/WebCore/page/PerformanceEntry.h
@@ -34,6 +34,7 @@
 #if ENABLE(WEB_TIMING)
 
 #include "Performance.h"
+#include <wtf/Optional.h>
 #include <wtf/RefCounted.h>
 #include <wtf/TypeCasts.h>
 #include <wtf/text/WTFString.h>
@@ -49,6 +50,17 @@ public:
     double startTime() const { return m_startTime; }
     double duration() const { return m_duration; }
 
+    enum class Type {
+        Navigation = 1 << 0,
+        Mark = 1 << 1,
+        Measure = 1 << 2,
+        Resource = 1 << 3,
+    };
+
+    Type type() const { return m_type; }
+
+    static std::optional<Type> parseEntryTypeString(const String& entryType);
+
     virtual bool isResource() const { return false; }
     virtual bool isMark() const { return false; }
     virtual bool isMeasure() const { return false; }
@@ -59,13 +71,14 @@ public:
     }
 
 protected:
-    PerformanceEntry(const String& name, const String& entryType, double startTime, double finishTime);
+    PerformanceEntry(Type, const String& name, const String& entryType, double startTime, double finishTime);
 
 private:
     const String m_name;
     const String m_entryType;
     const double m_startTime;
     const double m_duration;
+    const Type m_type;
 };
 
 } // namespace WebCore

Source/WebCore/page/PerformanceObserverEntryList.cpp
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "PerformanceObserverEntryList.h"
+
+#if ENABLE(WEB_TIMING)
+
+#include "PerformanceEntry.h"
+
+namespace WebCore {
+
+Ref<PerformanceObserverEntryList> PerformanceObserverEntryList::create(Vector<RefPtr<PerformanceEntry>>&& entries)
+{
+    return adoptRef(*new PerformanceObserverEntryList(WTFMove(entries)));
+}
+
+PerformanceObserverEntryList::PerformanceObserverEntryList(Vector<RefPtr<PerformanceEntry>>&& entries)
+    : m_entries(WTFMove(entries))
+{
+    ASSERT(!m_entries.isEmpty());
+
+    std::stable_sort(m_entries.begin(), m_entries.end(), PerformanceEntry::startTimeCompareLessThan);
+}
+
+Vector<RefPtr<PerformanceEntry>> PerformanceObserverEntryList::getEntriesByType(const String& entryType) const
+{
+    return getEntriesByName(String(), entryType);
+}
+
+Vector<RefPtr<PerformanceEntry>> PerformanceObserverEntryList::getEntriesByName(const String& name, const String& entryType) const
+{
+    Vector<RefPtr<PerformanceEntry>> entries;
+
+    // PerformanceObservers can only be registered for valid types.
+    // So if the incoming entryType is an unknown type, there will be no matches.
+    std::optional<PerformanceEntry::Type> type;
+    if (!entryType.isNull()) {
+        type = PerformanceEntry::parseEntryTypeString(entryType);
+        if (!type)
+            return entries;
+    }
+
+    for (auto& entry : m_entries) {
+        if (!name.isNull() && entry->name() != name)
+            continue;
+        if (type && entry->type() != *type)
+            continue;
+        entries.append(entry.copyRef());
+    }
+
+    return entries;
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(WEB_TIMING)

Balajims88/WPEWebKit

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@211406 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/PerformanceEntry.cpp
@@ -33,18 +33,41 @@
 
 #if ENABLE(WEB_TIMING)
 
+#include "RuntimeEnabledFeatures.h"
+
 namespace WebCore {
 
-PerformanceEntry::PerformanceEntry(const String& name, const String& entryType, double startTime, double finishTime)
+PerformanceEntry::PerformanceEntry(Type type, const String& name, const String& entryType, double startTime, double finishTime)
     : m_name(name)
     , m_entryType(entryType)
     , m_startTime(startTime)
     , m_duration(finishTime - startTime)
+    , m_type(type)
 {
 }
 
 PerformanceEntry::~PerformanceEntry()
 {
+}
+
+WTF::Optional<PerformanceEntry::Type> PerformanceEntry::parseEntryTypeString(const String& entryType)
+{
+    if (entryType == "navigation")
+        return WTF::Optional<Type>(Type::Navigation);
+
+    if (RuntimeEnabledFeatures::sharedFeatures().userTimingEnabled()) {
+        if (entryType == "mark")
+            return WTF::Optional<Type>(Type::Mark);
+        if (entryType == "measure")
+            return WTF::Optional<Type>(Type::Measure);
+    }
+
+    if (RuntimeEnabledFeatures::sharedFeatures().resourceTimingEnabled()) {
+        if (entryType == "resource")
+            return WTF::Optional<Type>(Type::Resource);
+    }
+
+    return WTF::Nullopt;
 }
 
 } // namespace WebCore

Source/WebCore/page/PerformanceEntry.h
@@ -35,6 +35,7 @@
 
 #include "Performance.h"
 #include <wtf/PassRefPtr.h>
+#include <wtf/Optional.h>
 #include <wtf/RefCounted.h>
 #include <wtf/TypeCasts.h>
 #include <wtf/text/WTFString.h>
@@ -50,6 +51,17 @@ public:
     double startTime() const { return m_startTime; }
     double duration() const { return m_duration; }
 
+    enum class Type {
+        Navigation = 1 << 0,
+        Mark = 1 << 1,
+        Measure = 1 << 2,
+        Resource = 1 << 3,
+    };
+
+    Type type() const { return m_type; }
+
+    static WTF::Optional<Type> parseEntryTypeString(const String& entryType);
+
     virtual bool isResource() const { return false; }
     virtual bool isMark() const { return false; }
     virtual bool isMeasure() const { return false; }
@@ -60,13 +72,14 @@ public:
     }
 
 protected:
-    PerformanceEntry(const String& name, const String& entryType, double startTime, double finishTime);
+    PerformanceEntry(Type, const String& name, const String& entryType, double startTime, double finishTime);
 
 private:
     const String m_name;
     const String m_entryType;
     const double m_startTime;
     const double m_duration;
+    const Type m_type;
 };
 
 } // namespace WebCore

Source/WebCore/page/PerformanceObserverEntryList.cpp
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "PerformanceObserverEntryList.h"
+
+#if ENABLE(WEB_TIMING)
+
+#include "PerformanceEntry.h"
+
+namespace WebCore {
+
+Ref<PerformanceObserverEntryList> PerformanceObserverEntryList::create(Vector<RefPtr<PerformanceEntry>>&& entries)
+{
+    return adoptRef(*new PerformanceObserverEntryList(WTFMove(entries)));
+}
+
+PerformanceObserverEntryList::PerformanceObserverEntryList(Vector<RefPtr<PerformanceEntry>>&& entries)
+    : m_entries(WTFMove(entries))
+{
+    ASSERT(!m_entries.isEmpty());
+
+    std::stable_sort(m_entries.begin(), m_entries.end(), PerformanceEntry::startTimeCompareLessThan);
+}
+
+Vector<RefPtr<PerformanceEntry>> PerformanceObserverEntryList::getEntriesByType(const String& entryType) const
+{
+    return getEntriesByName(String(), entryType);
+}
+
+Vector<RefPtr<PerformanceEntry>> PerformanceObserverEntryList::getEntriesByName(const String& name, const String& entryType) const
+{
+    Vector<RefPtr<PerformanceEntry>> entries;
+
+    // PerformanceObservers can only be registered for valid types.
+    // So if the incoming entryType is an unknown type, there will be no matches.
+    WTF::Optional<PerformanceEntry::Type> type;
+    if (!entryType.isNull()) {
+        type = PerformanceEntry::parseEntryTypeString(entryType);
+        if (!type)
+            return entries;
+    }
+
+    for (auto& entry : m_entries) {
+        if (!name.isNull() && entry->name() != name)
+            continue;
+        if (type && entry->type() != *type)
+            continue;
+        entries.append(entry.copyRef());
+    }
+
+    return entries;
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(WEB_TIMING)